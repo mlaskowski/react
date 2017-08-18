654e074ecc529613d6ca598f8ac7dce013006265
mlaskowski <freeq343@gmail.com>
2017-08-18 22:25:50 +0200

    react-2: starting making cross and circle game
     + add secound player

--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -2,25 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    // constructor() {
-        // super();
-        // this.state = {
-        //     value: null,
-        // };
-    // }
-
-    render() {
-        return (
-            <button className="square" onClick={() => this.props.onClick()}>
-                {/*onClick={() => this.setState({value: 'x'})}*/}
-            {this.props.value}
-            </button>
+// class Square extends React.Component {
+//     render() {
+//         return (
+//             <button className="square" onClick={() => this.props.onClick()}>
+//             {this.props.value}
+//             </button>
+//         );
+//     }
+// }
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
     );
-    }
 }
 
 class Board extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true,
+        };
+    }
+
     renderSquare(i) {
         return <Square
             value={this.state.squares[i]}
@@ -30,23 +37,17 @@ class Board extends React.Component {
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
-    }
-
-    constructor() {
-        super();
-        this.state = {
-            squares: Array(9).fill(null),
-        };
+        this.state.xIsNext ? squares[i] = 'X' : squares[i] = 'O' ;
+        this.setState({squares: squares,
+                       xIsNext: !this.state.xIsNext})
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player:';
 
         return (
             <div>
-            <div className="status">{status}</div>
+            <div className="status">{status}{this.state.xIsNext ? 'X' : 'O'}</div>
             <div className="board-row">
             {this.renderSquare(0)}
         {this.renderSquare(1)}
